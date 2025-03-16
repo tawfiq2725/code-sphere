fd27b0770a71bad50ec3cd5d68a454ab262ac9d4
Tawfiq Sirajudeen <tawfiqsiraj003003@gmail.com>
2025-03-16 20:09:09 +0530

    done

--- src/utils/backendUrl.ts
+++ src/utils/backendUrl.ts
@@ -4,3 +4,4 @@ export const backendUrl =
   process.env.NEXT_PUBLIC_NODE_ENV === "production"
     ? process.env.NEXT_PUBLIC_PRODUCTION_URL
     : process.env.NEXT_PUBLIC_BACKEND_URL;
+console.log(backendUrl);
